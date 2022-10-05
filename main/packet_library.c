2e756ff6c34d67aef56fd48ef78977c411017cf5
Your Name <pbibus@hotmail.com>
2022-10-04 23:32:24 -0700

    Updated callback names, set send and receive type holders to be the same callbacks, and setup send callbacks.

--- main/packet_library.c
+++ main/packet_library.c
@@ -4,65 +4,68 @@
 #include "packet_library.h"
 
 // Private helper static types
-static promisc_callback_setup_t callback_setup;
+static callback_setup_t promisc_callback_setup;
+static callback_setup_t send_callback_setup;
+static configuration_settings_t configuration_holder;
 
 // Private Helper functions
 static void promisc_simple_callback(void *buf, wifi_promiscuous_pkt_type_t type)
 {
     ESP_LOGI(LOGGING_TAG, "START SIMPLE POMISC CALLBACK");
     wifi_promiscuous_pkt_t *pkt = (wifi_promiscuous_pkt_t *)buf;
-    int length = pkt->rx_ctrl.sig_len;
+    //int length = pkt->rx_ctrl.sig_len; // TODO: Use for the payload callback
     wifi_mac_data_frame_t *frame = (wifi_mac_data_frame_t *)pkt->payload;
+
     // Do the simple callback and then each individual callback action
-    if (callback_setup.general_callback_is_set)
+    if (promisc_callback_setup.general_callback_is_set)
     {
-        callback_setup.general_callback(frame);
+        promisc_callback_setup.general_callback(frame);
     }
-    if (callback_setup.frame_control_callback_is_set)
+    if (promisc_callback_setup.frame_control_callback_is_set)
     {
-        callback_setup.frame_control_callback(&frame->frame_control);
+        promisc_callback_setup.frame_control_callback(&frame->frame_control);
     }
-    if (callback_setup.duration_id_callback_is_set)
+    if (promisc_callback_setup.duration_id_callback_is_set)
     {
-        callback_setup.duration_id_callback(&frame->duration_id);
+        promisc_callback_setup.duration_id_callback(&frame->duration_id);
     }
-    if (callback_setup.address_1_callback_is_set)
+    if (promisc_callback_setup.address_1_callback_is_set)
     {
-        callback_setup.address_1_callback(frame->address_1);
+        promisc_callback_setup.address_1_callback(frame->address_1);
     }
-    if (callback_setup.address_2_callback_is_set)
+    if (promisc_callback_setup.address_2_callback_is_set)
     {
-        callback_setup.address_2_callback(frame->address_2);
+        promisc_callback_setup.address_2_callback(frame->address_2);
     }
-    if (callback_setup.address_3_callback_is_set)
+    if (promisc_callback_setup.address_3_callback_is_set)
     {
-        callback_setup.address_3_callback(frame->address_3);
+        promisc_callback_setup.address_3_callback(frame->address_3);
     }
-    if (callback_setup.sequence_control_callback_is_set)
+    if (promisc_callback_setup.sequence_control_callback_is_set)
     {
-        callback_setup.sequence_control_callback(&frame->sequence_control);
+        promisc_callback_setup.sequence_control_callback(&frame->sequence_control);
     }
-    if (callback_setup.address_4_callback_is_set)
+    if (promisc_callback_setup.address_4_callback_is_set)
     {
-        callback_setup.address_4_callback(frame->address_4);
+        promisc_callback_setup.address_4_callback(frame->address_4);
     }
 }
 
 // Setup/configuration methods
 esp_err_t setup_wifi_simple()
 {
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    return setup_wifi_custom(cfg);
+}
+
+esp_err_t setup_wifi_custom(wifi_init_config_t config)
+{
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_LOGI(LOGGING_TAG, "SIMPLE WIFI INITIALIZED");
-    return ESP_OK;
-}
-
-esp_err_t setup_wifi_custom() // TODO
-{
+    ESP_ERROR_CHECK(esp_wifi_init(&config));
+    ESP_LOGI(LOGGING_TAG, "WIFI INITIALIZED");
     return ESP_OK;
 }
 
@@ -70,6 +73,8 @@ esp_err_t setup_sta_default()
 {
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
+    configuration_holder.wifi_interface = WIFI_IF_STA;
+    configuration_holder.wifi_interface_set = true;
     return ESP_OK;
 }
 
@@ -93,109 +98,257 @@ esp_err_t setup_promiscuous_simple()
 
 esp_err_t setup_promiscuous_simple_with_general_callback(packet_library_simple_callback_t simple_callback)
 {
-    callback_setup.general_callback = simple_callback;
-    callback_setup.general_callback_is_set = true;
-    ESP_ERROR_CHECK(setup_promiscuous_simple());
-    return ESP_OK;
+    promisc_callback_setup.general_callback = simple_callback;
+    promisc_callback_setup.general_callback_is_set = true;
+    return setup_promiscuous_simple();
 }
 
 // Send and receive full control
-esp_err_t send_packet_raw() // TODO: Call the send callback functions then send
+esp_err_t send_packet_raw(const void* buffer, int length, bool en_sys_seq)
 {
+    if(configuration_holder.wifi_interface_set != true)
+    {
+        return ESP_ERR_WIFI_IF;
+    }
+    esp_wifi_80211_tx(configuration_holder.wifi_interface, buffer, length, en_sys_seq);
     return ESP_OK;
 }
 
-esp_err_t send_packet_simple() // TODO: Call the send callback functions then send
+esp_err_t send_packet_simple(wifi_mac_data_frame_t* packet, int payload_length) 
 {
+    ESP_LOGI(LOGGING_TAG, "START SIMPLE SEND PACKET");
+    if(configuration_holder.wifi_interface_set != true)
+    {
+        return ESP_ERR_WIFI_IF;
+    }
+    int length = sizeof(packet) - sizeof(uint8_t *) + payload_length;
+    // Do the simple callback and then each individual callback action
+    // TODO: Update these for send
+    if (send_callback_setup.general_callback_is_set)
+    {
+        send_callback_setup.general_callback(packet);
+    }
+    if (send_callback_setup.frame_control_callback_is_set)
+    {
+        send_callback_setup.frame_control_callback(&packet->frame_control);
+    }
+    if (send_callback_setup.duration_id_callback_is_set)
+    {
+        send_callback_setup.duration_id_callback(&packet->duration_id);
+    }
+    if (send_callback_setup.address_1_callback_is_set)
+    {
+        send_callback_setup.address_1_callback(packet->address_1);
+    }
+    if (send_callback_setup.address_2_callback_is_set)
+    {
+        send_callback_setup.address_2_callback(packet->address_2);
+    }
+    if (send_callback_setup.address_3_callback_is_set)
+    {
+        send_callback_setup.address_3_callback(packet->address_3);
+    }
+    if (send_callback_setup.sequence_control_callback_is_set)
+    {
+        send_callback_setup.sequence_control_callback(&packet->sequence_control);
+    }
+    if (send_callback_setup.address_4_callback_is_set)
+    {
+        send_callback_setup.address_4_callback(packet->address_4);
+    }
+    esp_wifi_80211_tx(configuration_holder.wifi_interface, (void *)packet, length, false);
     return ESP_OK;
 }
 
-// Individual field callbacks/send options
-esp_err_t set_callback_frame_control(packet_library_frame_control_callback_t simple_callback)
+// **************************************************
+// Receive callback methods
+// **************************************************
+
+esp_err_t set_receive_callback_frame_control(packet_library_frame_control_callback_t simple_callback)
 {
-    callback_setup.frame_control_callback = simple_callback;
-    callback_setup.frame_control_callback_is_set = true;
+    promisc_callback_setup.frame_control_callback = simple_callback;
+    promisc_callback_setup.frame_control_callback_is_set = true;
     return ESP_OK;
 }
 
-esp_err_t set_callback_duration_id(packet_library_duration_id_callback_t simple_callback)
+esp_err_t set_receive_callback_duration_id(packet_library_duration_id_callback_t simple_callback)
 {
-    callback_setup.duration_id_callback = simple_callback;
-    callback_setup.duration_id_callback_is_set = true;
+    promisc_callback_setup.duration_id_callback = simple_callback;
+    promisc_callback_setup.duration_id_callback_is_set = true;
     return ESP_OK;
 }
 
-esp_err_t set_callback_address_1(packet_library_address_1_callback_t simple_callback)
+esp_err_t set_receive_callback_address_1(packet_library_address_1_callback_t simple_callback)
 {
-    callback_setup.address_1_callback = simple_callback;
-    callback_setup.address_1_callback_is_set = true;
+    promisc_callback_setup.address_1_callback = simple_callback;
+    promisc_callback_setup.address_1_callback_is_set = true;
     return ESP_OK;
 }
 
-esp_err_t set_callback_address_2(packet_library_address_2_callback_t simple_callback)
+esp_err_t set_receive_callback_address_2(packet_library_address_2_callback_t simple_callback)
 {
-    callback_setup.address_2_callback = simple_callback;
-    callback_setup.address_2_callback_is_set = true;
+    promisc_callback_setup.address_2_callback = simple_callback;
+    promisc_callback_setup.address_2_callback_is_set = true;
     return ESP_OK;
 }
 
-esp_err_t set_callback_address_3(packet_library_address_3_callback_t simple_callback)
+esp_err_t set_receive_callback_address_3(packet_library_address_3_callback_t simple_callback)
 {
-    callback_setup.address_3_callback = simple_callback;
-    callback_setup.address_3_callback_is_set = true;
+    promisc_callback_setup.address_3_callback = simple_callback;
+    promisc_callback_setup.address_3_callback_is_set = true;
     return ESP_OK;
 }
 
-esp_err_t set_callback_address_4(packet_library_address_4_callback_t simple_callback)
+esp_err_t set_receive_callback_address_4(packet_library_address_4_callback_t simple_callback)
 {
-    callback_setup.address_4_callback = simple_callback;
-    callback_setup.address_4_callback_is_set = true;
+    promisc_callback_setup.address_4_callback = simple_callback;
+    promisc_callback_setup.address_4_callback_is_set = true;
     return ESP_OK;
 }
 
-esp_err_t set_callback_sequence_control(packet_library_sequence_control_callback_t simple_callback)
+esp_err_t set_receive_callback_sequence_control(packet_library_sequence_control_callback_t simple_callback)
 {
-    callback_setup.sequence_control_callback = simple_callback;
-    callback_setup.sequence_control_callback_is_set = true;
+    promisc_callback_setup.sequence_control_callback = simple_callback;
+    promisc_callback_setup.sequence_control_callback_is_set = true;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback() // TODO
+esp_err_t remove_receive_callback_frame_control()
 {
+    promisc_callback_setup.frame_control_callback_is_set = false;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback_frame_control() // TODO
+esp_err_t remove_receive_callback_duration_id()
 {
+    promisc_callback_setup.duration_id_callback_is_set = false;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback_duration_id() // TODO
+esp_err_t remove_receive_callback_address_1()
 {
+    promisc_callback_setup.address_1_callback_is_set = false;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback_address_1() // TODO
+esp_err_t remove_receive_callback_address_2()
 {
+    promisc_callback_setup.address_2_callback_is_set = false;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback_address_2() // TODO
+esp_err_t remove_receive_callback_address_3()
 {
+    promisc_callback_setup.address_3_callback_is_set = false;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback_address_3() // TODO
+esp_err_t remove_receive_callback_address_4()
 {
+    promisc_callback_setup.address_4_callback_is_set = false;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback_address_4() // TODO
+esp_err_t remove_receive_callback_sequence_control()
 {
+    promisc_callback_setup.sequence_control_callback_is_set = false;
     return ESP_OK;
 }
 
-esp_err_t set_send_callback_sequence_control() // TODO
+
+// **************************************************
+// Send callback methods
+// **************************************************
+
+esp_err_t set_send_callback_frame_control(packet_library_frame_control_callback_t simple_callback)
 {
+    send_callback_setup.frame_control_callback = simple_callback;
+    send_callback_setup.frame_control_callback_is_set = true;
     return ESP_OK;
 }
+
+esp_err_t set_send_callback_duration_id(packet_library_duration_id_callback_t simple_callback)
+{
+    send_callback_setup.duration_id_callback = simple_callback;
+    send_callback_setup.duration_id_callback_is_set = true;
+    return ESP_OK;
+}
+
+esp_err_t set_send_callback_address_1(packet_library_address_1_callback_t simple_callback)
+{
+    send_callback_setup.address_1_callback = simple_callback;
+    send_callback_setup.address_1_callback_is_set = true;
+    return ESP_OK;
+}
+
+esp_err_t set_send_callback_address_2(packet_library_address_2_callback_t simple_callback)
+{
+    send_callback_setup.address_2_callback = simple_callback;
+    send_callback_setup.address_2_callback_is_set = true;
+    return ESP_OK;
+}
+
+esp_err_t set_send_callback_address_3(packet_library_address_3_callback_t simple_callback)
+{
+    send_callback_setup.address_3_callback = simple_callback;
+    send_callback_setup.address_3_callback_is_set = true;
+    return ESP_OK;
+}
+
+esp_err_t set_send_callback_address_4(packet_library_address_4_callback_t simple_callback)
+{
+    send_callback_setup.address_4_callback = simple_callback;
+    send_callback_setup.address_4_callback_is_set = true;
+    return ESP_OK;
+}
+
+esp_err_t set_send_callback_sequence_control(packet_library_sequence_control_callback_t simple_callback)
+{
+    send_callback_setup.sequence_control_callback = simple_callback;
+    send_callback_setup.sequence_control_callback_is_set = true;
+    return ESP_OK;
+}
+
+esp_err_t remove_send_callback_frame_control()
+{
+    send_callback_setup.frame_control_callback_is_set = false;
+    return ESP_OK;
+}
+
+esp_err_t remove_send_callback_duration_id()
+{
+    send_callback_setup.duration_id_callback_is_set = false;
+    return ESP_OK;
+}
+
+esp_err_t remove_send_callback_address_1()
+{
+    send_callback_setup.address_1_callback_is_set = false;
+    return ESP_OK;
+}
+
+esp_err_t remove_send_callback_address_2()
+{
+    send_callback_setup.address_2_callback_is_set = false;
+    return ESP_OK;
+}
+
+esp_err_t remove_send_callback_address_3()
+{
+    send_callback_setup.address_3_callback_is_set = false;
+    return ESP_OK;
+}
+
+esp_err_t remove_send_callback_address_4()
+{
+    send_callback_setup.address_4_callback_is_set = false;
+    return ESP_OK;
+}
+
+esp_err_t remove_send_callback_sequence_control()
+{
+    send_callback_setup.sequence_control_callback_is_set = false;
+    return ESP_OK;
+}
+
+
--- main/packet_library.h
+++ main/packet_library.h
@@ -5,6 +5,11 @@ static const char *LOGGING_TAG = "packet_library";
 
 // TypeDefs
 typedef struct {
+    wifi_interface_t wifi_interface;
+    bool wifi_interface_set;
+} configuration_settings_t;
+
+typedef struct {
     uint16_t frame_control;
     uint16_t duration_id;
     uint8_t address_1[6];
@@ -16,13 +21,13 @@ typedef struct {
 } wifi_mac_data_frame_t;
 
 typedef void (* packet_library_simple_callback_t)(wifi_mac_data_frame_t* packet);
-typedef void (* packet_library_frame_control_callback_t)(uint16_t* packet);
-typedef void (* packet_library_duration_id_callback_t)(uint16_t* packet);
-typedef void (* packet_library_address_1_callback_t)(uint8_t* packet);
-typedef void (* packet_library_address_2_callback_t)(uint8_t* packet);
-typedef void (* packet_library_address_3_callback_t)(uint8_t* packet);
-typedef void (* packet_library_address_4_callback_t)(uint8_t* packet);
-typedef void (* packet_library_sequence_control_callback_t)(uint16_t* packet);
+typedef void (* packet_library_frame_control_callback_t)(uint16_t* frame_control);
+typedef void (* packet_library_duration_id_callback_t)(uint16_t* duration_id);
+typedef void (* packet_library_address_1_callback_t)(uint8_t* address_1);
+typedef void (* packet_library_address_2_callback_t)(uint8_t* address_2);
+typedef void (* packet_library_address_3_callback_t)(uint8_t* address_3);
+typedef void (* packet_library_address_4_callback_t)(uint8_t* address_4);
+typedef void (* packet_library_sequence_control_callback_t)(uint16_t* sequence_control);
 
 typedef struct {
     bool general_callback_is_set;
@@ -44,37 +49,50 @@ typedef struct {
     // bool payload_callback_is_set;
     // packet_library_simple_callback_t payload_callback;
 
-} promisc_callback_setup_t;
+} callback_setup_t;
 
 // Setup/configuration methods
 esp_err_t setup_wifi_simple(); // This or custom version have to be called
-esp_err_t setup_wifi_custom(); 
+esp_err_t setup_wifi_custom(wifi_init_config_t config); 
 esp_err_t setup_sta_default();
 esp_err_t setup_promiscuous_default(wifi_promiscuous_cb_t callback);
 esp_err_t setup_promiscuous_simple(); // Enables individual section callbacks
 esp_err_t setup_promiscuous_simple_with_general_callback(packet_library_simple_callback_t simple_callback); 
 
 // Send full control
-esp_err_t send_packet_raw();
-esp_err_t send_packet_simple();
+esp_err_t send_packet_raw(const void* buffer, int length, bool en_sys_seq);
+esp_err_t send_packet_simple(wifi_mac_data_frame_t* packet, int payload_length);
 
 // Individual field callbacks/send options
-esp_err_t set_callback_frame_control(packet_library_frame_control_callback_t simple_callback);
-esp_err_t set_callback_duration_id(packet_library_duration_id_callback_t simple_callback);
-esp_err_t set_callback_address_1(packet_library_address_1_callback_t simple_callback);
-esp_err_t set_callback_address_2(packet_library_address_2_callback_t simple_callback);
-esp_err_t set_callback_address_3(packet_library_address_3_callback_t simple_callback);
-esp_err_t set_callback_address_4(packet_library_address_4_callback_t simple_callback);
-esp_err_t set_callback_sequence_control(packet_library_sequence_control_callback_t simple_callback);
-// TODO add payload callback/send, will need to include data length stuff
+esp_err_t set_receive_callback_frame_control(packet_library_frame_control_callback_t simple_callback);
+esp_err_t set_receive_callback_duration_id(packet_library_duration_id_callback_t simple_callback);
+esp_err_t set_receive_callback_address_1(packet_library_address_1_callback_t simple_callback);
+esp_err_t set_receive_callback_address_2(packet_library_address_2_callback_t simple_callback);
+esp_err_t set_receive_callback_address_3(packet_library_address_3_callback_t simple_callback);
+esp_err_t set_receive_callback_address_4(packet_library_address_4_callback_t simple_callback);
+esp_err_t set_receive_callback_sequence_control(packet_library_sequence_control_callback_t simple_callback);
+esp_err_t remove_receive_callback_frame_control();
+esp_err_t remove_receive_callback_duration_id();
+esp_err_t remove_receive_callback_address_1();
+esp_err_t remove_receive_callback_address_2();
+esp_err_t remove_receive_callback_address_3();
+esp_err_t remove_receive_callback_address_4();
+esp_err_t remove_receive_callback_sequence_control();
+// TODO: add payload callback/send, will need to include data length stuff
 
-esp_err_t set_send_callback();
-esp_err_t set_send_callback_frame_control();
-esp_err_t set_send_callback_duration_id();
-esp_err_t set_send_callback_address_1();
-esp_err_t set_send_callback_address_2();
-esp_err_t set_send_callback_address_3();
-esp_err_t set_send_callback_address_4();
-esp_err_t set_send_callback_sequence_control();
+esp_err_t set_send_callback_frame_control(packet_library_frame_control_callback_t simple_callback);
+esp_err_t set_send_callback_duration_id(packet_library_duration_id_callback_t simple_callback);
+esp_err_t set_send_callback_address_1(packet_library_address_1_callback_t simple_callback);
+esp_err_t set_send_callback_address_2(packet_library_address_2_callback_t simple_callback);
+esp_err_t set_send_callback_address_3(packet_library_address_3_callback_t simple_callback);
+esp_err_t set_send_callback_address_4(packet_library_address_4_callback_t simple_callback);
+esp_err_t set_send_callback_sequence_control(packet_library_sequence_control_callback_t simple_callback);
+esp_err_t remove_send_callback_frame_control();
+esp_err_t remove_send_callback_duration_id();
+esp_err_t remove_send_callback_address_1();
+esp_err_t remove_send_callback_address_2();
+esp_err_t remove_send_callback_address_3();
+esp_err_t remove_send_callback_address_4();
+esp_err_t remove_send_callback_sequence_control();
 
 #endif
