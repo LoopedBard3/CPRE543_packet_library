dbf22f2364606dae6ec8f7c2d5440fb9443eabcb
Your Name <pbibus@hotmail.com>
2022-10-15 21:23:54 -0700

    Final version for Checkpoint 2.

--- main/library_test.c
+++ main/library_test.c
@@ -38,6 +38,18 @@ static void double_duration_id_callback(uint16_t* duration_id)
     *duration_id = *duration_id * 2;    
     ESP_LOGI(LOGGING_TAG, "CB: DI, NV: %04X", *duration_id);
 }
+static void set_duration_id_callback(uint16_t* duration_id)
+{
+    if(*duration_id == 0xBEEE)
+    {
+        *duration_id = 0xCAB0;
+    } 
+    else
+    {
+        *duration_id = 0xBEEE;
+    }   
+    ESP_LOGI(LOGGING_TAG, "CB: DI, NV: %04X", *duration_id);
+}
 static void double_address_1_callback(uint8_t address_1[6])
 {
     address_1[1] = address_1[0] * 2;    
@@ -56,6 +68,19 @@ static void double_address_3_callback(uint8_t address_3[6])
 static void double_sequence_control_callback(uint16_t* sequence_control)
 {
     *sequence_control = *sequence_control * 2;    
+    ESP_LOGI(LOGGING_TAG, "CB: SC, NV: %04X", *sequence_control);
+}
+static void set_sequence_control_callback(uint16_t* sequence_control)
+{
+    if(*sequence_control == 0xBEEE)
+    {
+        *sequence_control = 0xCAB0;
+    } 
+    else
+    {
+        *sequence_control = 0xBEEE;
+    }
+        
     ESP_LOGI(LOGGING_TAG, "CB: SC, NV: %04X", *sequence_control);
 }
 static void double_address_4_callback(uint8_t address_4[6])
@@ -117,11 +142,11 @@ static void library_test(void)
         int payload_length = 6;
         wifi_mac_data_frame_t* pkt = alloc_packet_custom(
             0x0008, //  bits are read in opposite order 
-            4, 
+            0xFA, 
             (uint8_t []){ 0x10, 0x11, 0x12, 0x13, 0x14, 0x15 },
             (uint8_t []){ 0x20, 0x21, 0x22, 0x23, 0x24, 0x25 },
             (uint8_t []){ 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 },
-            7,
+            0xFA,
             (uint8_t []){ 0x40, 0x41, 0x42, 0x43, 0x44, 0x45 },
             payload_length,
             payload
@@ -131,12 +156,12 @@ static void library_test(void)
         if(DO_INDIVIDUAL_CALLBACK_TEST)
         {
             //ESP_ERROR_CHECK(set_send_callback_frame_control(&double_frame_control_callback));
-            ESP_ERROR_CHECK(set_send_callback_duration_id(&double_duration_id_callback));
+            ESP_ERROR_CHECK(set_send_callback_duration_id(&set_duration_id_callback));
             ESP_ERROR_CHECK(set_send_callback_address_1(&double_address_1_callback));
             ESP_ERROR_CHECK(set_send_callback_address_2(&double_address_2_callback));
             ESP_ERROR_CHECK(set_send_callback_address_3(&double_address_3_callback));
             ESP_ERROR_CHECK(set_send_callback_address_4(&double_address_4_callback));
-            ESP_ERROR_CHECK(set_send_callback_sequence_control(&double_sequence_control_callback));
+            ESP_ERROR_CHECK(set_send_callback_sequence_control(&set_sequence_control_callback));
             ESP_ERROR_CHECK(set_send_callback_payload(&payload_callback)); // Payload includes FCS which is not included in the send setup (explains size disparity)
         }
 
